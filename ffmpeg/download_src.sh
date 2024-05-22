6b2cbb6a3ffa4d17a2d36876bb4ce18049a1300f
Alexander Alekhin <alexander.a.alekhin@gmail.com>
2024-05-22 05:07:30 +0000

    4.x: bump FFmpeg wrapper version (2024.05.0), ffmpeg 4.4.4, libvpx 1.14.0, aom 3.9.0

--- ffmpeg/download_src.sh
+++ ffmpeg/download_src.sh
@@ -20,14 +20,14 @@ update() {
 mkdir -p ../build
 (
 cd ../build
-# https://github.com/FFmpeg/FFmpeg/releases
+# https://github.com/FFmpeg/FFmpeg/tags
 update ffmpeg git://source.ffmpeg.org/ffmpeg.git n4.4.4
 # https://github.com/cisco/openh264/releases
 update openh264 https://github.com/cisco/openh264.git v1.8.0
 # https://chromium.googlesource.com/webm/libvpx.git
-update libvpx https://chromium.googlesource.com/webm/libvpx.git v1.13.1
+update libvpx https://chromium.googlesource.com/webm/libvpx.git v1.14.0
 # https://aomedia.googlesource.com/aom
-update aom https://aomedia.googlesource.com/aom v3.8.0
+update aom https://aomedia.googlesource.com/aom v3.9.0
 )
 
 # Pack all source code / build scripts
